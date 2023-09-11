470b8e2abbb54507c98ca73e207e5254c9f46120
Vinicius Rodrigues Duarte <viniciusrduarte8@gmail.com>
2023-09-11 12:42:30 -0300

    Curso NodeJS III

--- mmorpg_got/config/dbConnection.js
+++ mmorpg_got/config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connMongoDB() {
   try{
-    await mongoose.connect(/*process.env.MONGO_URI*/'mongodb+srv://viniciusrduarte8:mdbVrd6870@cluster0.gelsitl.mongodb.net/got?retryWrites=true&w=majority');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Conectado ao MongoDB!')
   } catch(error) {
     console.log('Erro ao conectar ao MongoDB');
